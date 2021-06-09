245f49776c53ed25e7ca9fbddc502ea853360698
aditya muley <aditya3muley@gmail.com>
2021-06-09 01:47:50 -0700

    read csv content and store it in type struct question

--- Quiz game/quiz.go
+++ Quiz game/quiz.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -18,8 +20,40 @@ type FileNotPresent struct {
 	What string
 }
 
+type Questions struct {
+	expression string
+	answer     string
+}
+
 func (e *FileNotPresent) Error() string {
 	return fmt.Sprintf("Error:- at %v, %s", e.When.Format("01-JAN-2006 15:04:00"), e.What)
+}
+
+func readFile(questions_file string) []Questions {
+	var questions_list []Questions
+	csv_file, err := os.Open(questions_file)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer csv_file.Close()
+
+	quiz_data, err := csv.NewReader(csv_file).ReadAll()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, row := range quiz_data {
+		individual_question := Questions{
+			expression: row[0],
+			answer:     row[1],
+		}
+		questions_list = append(questions_list, individual_question)
+	}
+
+	return questions_list
 }
 
 func main() {
@@ -30,5 +64,6 @@ func main() {
 		}
 		fmt.Println(err)
 	}
-	fmt.Println(*fileName == "")
+	// readFile(*fileName)
+	fmt.Println(readFile(*fileName))
 }
