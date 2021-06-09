4bcfb9044701473ed8feb178c1a96a8dc12b8802
aditya muley <aditya3muley@gmail.com>
2021-06-09 00:30:18 -0700

    added blank file name error handling functionality

--- Quiz game/quiz.go
+++ Quiz game/quiz.go
@@ -1,11 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var fileName *string
 
 func init() {
-	//initialization code
+	fileName = flag.String("fname", "", "CSV File containing quiz questions")
+	flag.Parse()
+}
+
+type FileNotPresent struct {
+	When time.Time
+	What string
+}
+
+func (e *FileNotPresent) Error() string {
+	return fmt.Sprintf("Error:- at %v, %s", e.When.Format("01-JAN-2006 15:04:00"), e.What)
 }
 
 func main() {
-	fmt.Println("Initial setup")
+	if *fileName == "" {
+		err := &FileNotPresent{
+			time.Now(),
+			"Please provide file name using flag -fname i.e( -fname 'filename')",
+		}
+		fmt.Println(err)
+	}
+	fmt.Println(*fileName == "")
 }
